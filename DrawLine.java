96b7fe1cb666d21154b8fc7c6426a1166d3b01ac
Yoga Sri <40590412+yogasrim31@users.noreply.github.com>
2018-08-18 20:30:49 +0530

    Add files via upload

---
+++ DrawLine.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+ 
+ 
+public class DrawLine extends Applet{
+ 
+	public void paint(Graphics g){
+		
+	
+		
+		
+	
+		g.drawLine(10,10,50,10);
+	}
+}
